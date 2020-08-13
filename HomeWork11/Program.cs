157600ad972c174abfd7aaa6e8558bd3488138b7
fanat85 <uyriy18@gmail.com>
2020-08-13 16:04:59 +0300

    Add project files.

---
+++ HomeWork11/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams
+{
+    class Program
+    {
+        public static void WriteFile(string path)                          // Method to write Data into file
+        {
+            try                                                            // If file doesn't exist handle the exeption
+            {
+
+
+                using (FileStream fs = new FileStream(path, FileMode.CreateNew)) // create new stream with CreateMode
+                {
+                    using (var sw = new StreamWriter(fs, Encoding.Unicode)) // create StreamWriter to wrire the Data
+                    {
+                        const int rows = 2;                                 // write the Data
+                        const int cols = 3;
+                        double[,] doubleArray = { { 1.1, 2.2, 3.3 }, { 4.4, 5.5, 6.6 } };
+                        int[,] intArray = new int[rows, cols] { { 1, 2, 3 }, { 4, 5, 6 } };
+                        sw.WriteLine("Ivanov Ivan Ivanich, 01.01.2001");
+                        for (int i = 0; i < 2; i++)
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                sw.Write(doubleArray[i, j] + "  ");
+                            }
+                            sw.WriteLine();
+                        }
+                        foreach (int item in intArray)
+                        {
+                            sw.Write(item + " ");
+                        }
+                        sw.WriteLine();                                                   // pass the Data into file
+                        sw.WriteLine($"Integer array has {rows} rows, and {cols} cols");
+                        sw.WriteLine(DateTime.Now);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            };
+        }
+        public static void ReadFile(string path)                                // Method to read the Data from the file
+        {
+            using(FileStream fs = new FileStream(path, FileMode.Open))          // Create File stream with open mode
+            {
+                using(StreamReader sr = new StreamReader(fs, Encoding.Unicode)) // Use StreamReader to read the Data from file
+                {
+                    Console.WriteLine(sr.ReadToEnd());                          // read to the end of file
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string path = @"test.dat";            // file's full name
+            while (true)
+            {
+                Console.WriteLine("To write data in to file - press 1, to read data from file press 2");
+                string choose = Console.ReadLine();
+               
+                if (choose == "1")
+                {
+                    WriteFile(path);                      // invoke write method
+                }
+                else if(choose == "2")
+                {
+                    ReadFile(path);                       // invoke read method
+                }
+                else Console.WriteLine("Invalid value");
+            }
+        }
+    }
+   
+}
---
+++ RegEx/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegEx
+{
+    class Program
+    {
+        
+
+        public static string ReadFile(string path)  // Read text from data
+        {
+            using(FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                using(StreamReader sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    string text = sr.ReadToEnd();
+                    return text;
+                }
+            }
+        }
+        public static void Writefile(string text)  //write to txt file
+        {
+            using(FileStream fs = new FileStream("Result_Text.txt", FileMode.Create))
+            {
+                using(StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    StringBuilder resultText = new StringBuilder(text.Length);  // create string builder for string reverse
+                    for( int i = text.Length - 1; i > 0; i--)
+                    {
+                        resultText.Append(text[i]);                             // put string in stringbuilder in reverse order
+                    }
+                    sw.WriteLine(resultText);
+                    Console.WriteLine("------------------Text was saved------------------");
+                    Console.WriteLine("Result text :\n\n" + resultText);        // show the result
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string path = @"text1.txt";                       // path to .txt file
+            string text = ReadFile(path);
+            string pattern1 = @"\w+\s{2,}\w+";                         // create pattern to remove all unnecessary spaces
+            string resultText = Regex.Replace(text, "public", "private");  // change "public" to "private"
+            string[] resultArray = resultText.Split(' ');     // turn string to array by spliting it with spases
+            resultText = "";                         // delete the result string
+            foreach(string item in resultArray)
+            {
+                string temp = "";
+                if (Regex.IsMatch(item, @"\w+"))  //if the item consists 2 or more leters
+                {
+                    temp = item.ToUpper();           //to upper case
+                    resultText += temp + " ";              //add to result string word in upper case
+                }
+                if(Regex.IsMatch(item, @"\d,\w*"))
+                {
+                    resultText += item + " ";        //add to result string not changed word
+                }       
+            }
+           
+            Console.WriteLine(text);                 //show original text
+            Writefile(resultText);                   
+        }
+    }
+}
